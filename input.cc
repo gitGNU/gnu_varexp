a2c14d85f92cb5bae8787fdd7a447b7e15cf4cf8
Peter Simons <simons@cryp.to>
2002-01-01 13:51:25 +0000

    - Swapped the semantics of 'p' and 'begin' to match the other routines. - Implemented error reporting via the varexp::error exception.

--- input.cc
+++ input.cc
@@ -5,125 +5,123 @@ namespace varexp
     namespace internal
         {
         size_t input(const char* begin, const char* end,
-                       const var_config_t* config,
-                       const char_class_t nameclass, var_cb_t lookup,
-                       void* lookup_context,
-                       tokenbuf_t* output, int current_index,
-                       size_t recursion_level, int* rel_lookup_flag)
+                     const var_config_t* config,
+                     const char_class_t nameclass, var_cb_t lookup,
+                     void* lookup_context,
+                     tokenbuf_t* output, int current_index,
+                     size_t recursion_level, int* rel_lookup_flag)
             {
             const char* p = begin;
-            size_t rc, rc2;
-            tokenbuf_t result;
-            int start, step, stop, open_end;
-            int i;
-            int output_backup;
-            struct wrapper_context wcon;
-            int my_rel_lookup_flag;
-            int original_rel_lookup_state;
-            int loop_limit_length;
-
-            if (rel_lookup_flag == NULL)
+            try
                 {
-                rel_lookup_flag  = &my_rel_lookup_flag;
-                *rel_lookup_flag = 0;
-                }
+                size_t rc, rc2;
+                tokenbuf_t result;
+                int start, step, stop, open_end;
+                int i;
+                int output_backup;
+                struct wrapper_context wcon;
+                int my_rel_lookup_flag;
+                int original_rel_lookup_state;
+                int loop_limit_length;
 
-            do
-                {
-                if (begin != end && config->startindex && *begin == config->startindex)
+                if (rel_lookup_flag == NULL)
                     {
-                    original_rel_lookup_state = *rel_lookup_flag;
-                    loop_limit_length = -1;
-                    wcon.lookup = lookup;
-                    wcon.context = lookup_context;
-                    wcon.rel_lookup_flag = rel_lookup_flag;
-                    begin++;
-                    start = 0;
-                    step  = 1;
-                    stop  = 0;
-                    open_end = 1;
-                    rc = 0;
-                    output_backup = 0;
-                  re_loop:
-                    for (i = start;
-                         (open_end  && (loop_limit_length < 0 || *rel_lookup_flag > original_rel_lookup_state)) ||
-                             (!open_end && i <= stop);
-                         i += step)
+                    rel_lookup_flag  = &my_rel_lookup_flag;
+                    *rel_lookup_flag = 0;
+                    }
+
+                do
+                    {
+                    if (p != end && config->startindex && *p == config->startindex)
                         {
-                        *rel_lookup_flag = original_rel_lookup_state;
-                        output_backup = output->end - output->begin;
-                        rc = input(begin, end, config, nameclass, &lookup_wrapper,
-                                   &wcon, output, i, recursion_level+1, rel_lookup_flag);
-                        if (begin[rc] != config->endindex)
+                        original_rel_lookup_state = *rel_lookup_flag;
+                        loop_limit_length = -1;
+                        wcon.lookup = lookup;
+                        wcon.context = lookup_context;
+                        wcon.rel_lookup_flag = rel_lookup_flag;
+                        p++;
+                        start = 0;
+                        step  = 1;
+                        stop  = 0;
+                        open_end = 1;
+                        rc = 0;
+                        output_backup = 0;
+                      re_loop:
+                        for (i = start;
+                             (open_end  && (loop_limit_length < 0 || *rel_lookup_flag > original_rel_lookup_state)) ||
+                                 (!open_end && i <= stop);
+                             i += step)
                             {
-                            throw unterminated_loop_construct();
-                            }
-                        if (loop_limit_length < 0)
-                            {
-                            rc2 = loop_limits(begin + rc + 1, end, config, nameclass,
-                                              lookup, lookup_context, &start, &step,
-                                              &stop, &open_end);
-                            if (rc2 == 0)
+                            *rel_lookup_flag = original_rel_lookup_state;
+                            output_backup = output->end - output->begin;
+                            rc = input(p, end, config, nameclass, &lookup_wrapper,
+                                       &wcon, output, i, recursion_level+1, rel_lookup_flag);
+                            if (p[rc] != config->endindex)
                                 {
-                                loop_limit_length = 0;
+                                throw unterminated_loop_construct();
                                 }
-                            else if (rc2 > 0)
+                            if (loop_limit_length < 0)
                                 {
-                                loop_limit_length = rc2;
-                                output->end = output->begin + output_backup;
-                                goto re_loop;
+                                rc2 = loop_limits(p + rc + 1, end, config, nameclass,
+                                                  lookup, lookup_context, &start, &step,
+                                                  &stop, &open_end);
+                                if (rc2 == 0)
+                                    {
+                                    loop_limit_length = 0;
+                                    }
+                                else if (rc2 > 0)
+                                    {
+                                    loop_limit_length = rc2;
+                                    output->end = output->begin + output_backup;
+                                    goto re_loop;
+                                    }
                                 }
                             }
+                        if (open_end)
+                            output->end = output->begin + output_backup;
+                        else
+                            *rel_lookup_flag = original_rel_lookup_state;
+                        p += rc;
+                        p++;
+                        p += loop_limit_length;
+                        continue;
                         }
-                    if (open_end)
-                        output->end = output->begin + output_backup;
-                    else
-                        *rel_lookup_flag = original_rel_lookup_state;
-                    begin += rc;
-                    begin++;
-                    begin += loop_limit_length;
-                    continue;
+
+                    rc = text(p, end, config->varinit, config->startindex,
+                              config->endindex, config->escape);
+                    if (rc > 0)
+                        {
+                        output->append(p, rc);
+                        p += rc;
+                        continue;
+                        }
+
+                    rc = variable(p, end, config, nameclass, lookup,
+                                  lookup_context, &result,
+                                  current_index, rel_lookup_flag);
+                    if (rc > 0)
+                        {
+                        output->append(result.begin, result.end - result.begin);
+                        p += rc;
+                        continue;
+                        }
+                    }
+                while (p != end && rc > 0);
+
+                if (recursion_level == 0 && p != end)
+                    {
+                    throw input_isnt_text_nor_variable();
                     }
 
-                rc = text(begin, end, config->varinit, config->startindex,
-                          config->endindex, config->escape);
-                if (rc > 0)
-                    {
-                    output->append(begin, rc);
-                    begin += rc;
-                    continue;
-                    }
-                else
-                    if (rc < 0)
-                        goto error_return;
-
-                rc = variable(begin, end, config, nameclass, lookup,
-                              lookup_context, &result,
-                              current_index, rel_lookup_flag);
-                if (rc > 0)
-                    {
-                    output->append(result.begin, result.end - result.begin);
-                    begin += rc;
-                    continue;
-                    }
-                if (rc < 0)
-                    goto error_return;
+                return p - begin;
                 }
-            while (begin != end && rc > 0);
-
-            if (recursion_level == 0 && begin != end)
+            catch(error& e)
                 {
-                throw input_isnt_text_nor_variable();
+                e.begin   = begin;
+                e.current = p;
+                e.end     = end;
+                throw;
                 }
-
-            return begin - p;
-
-          error_return:
-            output->clear();
-            output->begin = p;
-            output->end = begin;
-            output->buffer_size = 0;
-            return rc;
             }
         }
     }

22cba5ad03128e91113dbc219ab3933a8279bee2
Peter Simons <simons@cryp.to>
2002-01-02 23:03:51 +0000

    - Removed unused system includes. - Don't mention namespace 'std' explicitely; we're 'using" it anyway.

--- internal.hh
+++ internal.hh
@@ -1,11 +1,7 @@
 #ifndef LIB_VARIABLE_EXPAND_INTERNAL_HH
 #define LIB_VARIABLE_EXPAND_INTERNAL_HH
 
-#include <cassert>
 #include <cctype>
-#include <memory>
-#include <cstdio>
-#include <cerrno>
 #include "varexp.hh"
 using namespace std;
 
@@ -22,24 +18,24 @@ namespace varexp
         class parser
             {
           protected:
-            friend void varexp::expand(const std::string& input,
-                                       std::string& result, callback_t& lookup,
+            friend void varexp::expand(const string& input,
+                                       string& result, callback_t& lookup,
                                        const config_t* config);
 
             parser(const config_t& _config, callback_t& _lookup);
             size_t input(const char* begin, const char* end, string& result);
 
           private:
-            size_t command(const char *begin, const char *end, std::string& result);
-            size_t expression(const char *begin, const char *end, std::string& result);
+            size_t command(const char *begin, const char *end, string& result);
+            size_t expression(const char *begin, const char *end, string& result);
             size_t loop_limits(const char *begin, const char *end, int& start, int& step, int& stop, bool& open_end);
             size_t num_exp_read_operand(const char* begin, const char* end, int& result);
             size_t num_exp(const char *begin, const char *end, int& result);
             size_t number(const char *begin, const char *end, unsigned int& num);
             size_t exptext(const char *begin, const char *end);
-            size_t exptext_or_variable(const char *begin, const char *end, std::string& result);
-            size_t substext_or_variable(const char *begin, const char *end, std::string& result);
-            size_t variable(const char *begin, const char *end, std::string& result);
+            size_t exptext_or_variable(const char *begin, const char *end, string& result);
+            size_t substext_or_variable(const char *begin, const char *end, string& result);
+            size_t variable(const char *begin, const char *end, string& result);
             size_t varname(const char *begin, const char *end);
             size_t text(const char *begin, const char *end);
             size_t substext(const char* begin, const char* end);
@@ -53,10 +49,10 @@ namespace varexp
 
         // Helper routines used by the parser.
 
-        void cut_out_offset(std::string& data, unsigned int num1, unsigned int num2, bool is_range);
-        void padding(std::string& data, unsigned int width, const std::string& fillstring, char position);
-        void search_and_replace(std::string& data, const std::string& search, const std::string& replace, const std::string& flags);
-        void transpose(std::string& data, const std::string& srcclass, const std::string& dstclass);
+        void cut_out_offset(string& data, unsigned int num1, unsigned int num2, bool is_range);
+        void padding(string& data, unsigned int width, const string& fillstring, char position);
+        void search_and_replace(string& data, const string& search, const string& replace, const string& flags);
+        void transpose(string& data, const string& srcclass, const string& dstclass);
         void expand_character_class(const char *desc, char_class_t char_class);
         }
     }

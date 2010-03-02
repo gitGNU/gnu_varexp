442c6aa37d4b03f08efacd91bbcfc08362a05e64
Peter Simons <simons@cryp.to>
2010-03-02 15:48:11 +0100

    post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -2,7 +2,7 @@
 
 gnu_rel_host    := dl.sv.nongnu.org
 upload_dest_dir_:= /releases/varexp/
-old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash   := 16f33c5cbef96ef845bc531d417205f3
 gpg_key_ID      := 99089D72
 url_dir_list    := http://download.savannah.nongnu.org/releases/varexp
 today           := $(date "+%Y-%m-%d")
